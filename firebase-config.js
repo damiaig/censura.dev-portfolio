0ff0a8c96b30d9ffe633de7ca962efa397edf245
damiaig <damilolaaig@gmail.com>
2025-05-08 10:57:56 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
 // firebase-config.js
 
 // Import Firebase modules
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // Your Firebase config
 const firebaseConfig = {
@@ -17,7 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase and Firestore
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 // Export db so it can be used in other scripts
-export { db };
+ 
